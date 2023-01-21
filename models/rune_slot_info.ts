964493aeeb416d50bfdd8e3ef7407d1c119620da
Moreal <dev.moreal@gmail.com>
2023-01-21 17:22:30 +0900

    Implement `RuneSlotInfo` deserialization

--- models/rune_slot_info.ts
+++ models/rune_slot_info.ts
@@ -1,4 +1,7 @@
-import { EncodableArray } from "https://esm.sh/v102/bencodex@0.1.2/lib/index";
+import {
+  BencodexValue,
+  EncodableArray,
+} from "https://esm.sh/v102/bencodex@0.1.2/lib/index";
 import { Bencodexable, toBencodex } from "./mod.ts";
 
 export class RuneSlotInfo implements Bencodexable {
@@ -15,5 +18,31 @@ export class RuneSlotInfo implements Bencodexable {
 
   [toBencodex](): EncodableArray {
     return [this.slotIndex.toString(), this.runeId.toString()];
+  }
+
+  static fromBencodex(value: BencodexValue): RuneSlotInfo {
+    if (!(value instanceof Array)) {
+      throw TypeError("RuneSlotInfo.fromBencodex() receives only array.");
+    }
+
+    if (typeof value[0] !== "string") {
+      throw new TypeError(
+        "RuneSlotInfo's first element should be `string` type.",
+      );
+    }
+
+    if (typeof value[1] !== "string") {
+      throw new TypeError(
+        "RuneSlotInfo's second element should be `string` type.",
+      );
+    }
+
+    const slotIndex = BigInt(value[0]);
+    const runeId = BigInt(value[1]);
+
+    return new RuneSlotInfo({
+      slotIndex,
+      runeId,
+    });
   }
 }
